b9d40f86f0c066e0f6bce265e4aa8d0f1a58136e
Pierre-Louis Serré <pierre-louisserre@device-85.home>
2025-08-19 16:21:44 +0200

    AssertFormPage assert created

--- dotnet/Falcon/FalconEngineTest/DomParsing/Parser/InputParserTest.cs
+++ dotnet/Falcon/FalconEngineTest/DomParsing/Parser/InputParserTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using FalconEngine.Models;
 using FalconEngineTest.Utils;
+using FalconEngineTest.Utils.AssertHtml;
 using FalconEngineTest.Utils.HtmlData;
 
 namespace FalconEngineTest.DomParsing.Parser
@@ -18,16 +19,7 @@ namespace FalconEngineTest.DomParsing.Parser
 
             var tag = inputTagParser.Parse(html);
 
-            Assert.Equal(NameTagEnum.input, tag.NameTag);
-            Assert.Null(tag.Content);
-            Assert.Equal(FamilyAttributeEnum.type.ToString(), tag.Attributes[0].FamilyAttribute);
-            Assert.Equal("Submit", tag.Attributes[0].Value);
-            Assert.Equal(FamilyAttributeEnum.value.ToString(), tag.Attributes[1].FamilyAttribute);
-            Assert.Equal("Submit", tag.Attributes[1].Value);
-            Assert.Equal(TagFamilyEnum.NoEnd, tag.TagFamily);
-            Assert.Equal("<input type=\"Submit\" value=\"Submit\"/>", tag.TagStart);
-            Assert.Null(tag.TagEnd);
-            Assert.Null(tag.Children);
+            AssertFormPage.AssertInputSubmit(tag);
         }
 
         [Fact]
@@ -38,20 +30,7 @@ namespace FalconEngineTest.DomParsing.Parser
 
             var tag = inputTagParser.Parse(html);
 
-            Assert.Equal(NameTagEnum.input, tag.NameTag);
-            Assert.Null(tag.Content);
-            Assert.Equal(FamilyAttributeEnum.type.ToString(), tag.Attributes[0].FamilyAttribute);
-            Assert.Equal("file", tag.Attributes[0].Value);
-            Assert.Equal(FamilyAttributeEnum.id.ToString(), tag.Attributes[1].FamilyAttribute);
-            Assert.Equal("avatar", tag.Attributes[1].Value);
-            Assert.Equal(FamilyAttributeEnum.name.ToString(), tag.Attributes[2].FamilyAttribute);
-            Assert.Equal("avatar", tag.Attributes[1].Value);
-            Assert.Equal(FamilyAttributeEnum.accept.ToString(), tag.Attributes[3].FamilyAttribute);
-            Assert.Equal(".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document", tag.Attributes[3].Value);
-            Assert.Equal(TagFamilyEnum.NoEnd, tag.TagFamily);
-            Assert.Equal("<input type=\"file\" id=\"avatar\" name=\"avatar\" accept=\".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document\">", tag.TagStart);
-            Assert.Null(tag.TagEnd);
-            Assert.Null(tag.Children);
+            AssertFormPage.AssertInputFile(tag);
         }
 
         [Fact]
@@ -62,22 +41,7 @@ namespace FalconEngineTest.DomParsing.Parser
 
             var tag = inputTagParser.Parse(html);
 
-            Assert.Equal(NameTagEnum.input, tag.NameTag);
-            Assert.Null(tag.Content);
-            Assert.Equal(FamilyAttributeEnum.type.ToString(), tag.Attributes[0].FamilyAttribute);
-            Assert.Equal("radio", tag.Attributes[0].Value);
-            Assert.Equal(FamilyAttributeEnum.id.ToString(), tag.Attributes[1].FamilyAttribute);
-            Assert.Equal("rgender", tag.Attributes[1].Value);
-            Assert.Equal(FamilyAttributeEnum.name.ToString(), tag.Attributes[2].FamilyAttribute);
-            Assert.Equal("rgender", tag.Attributes[1].Value);
-            Assert.Equal(FamilyAttributeEnum.value.ToString(), tag.Attributes[3].FamilyAttribute);
-            Assert.Equal("undefined", tag.Attributes[3].Value);
-            Assert.Equal(FamilyAttributeEnum.checkedAttr.ToString(), tag.Attributes[4].FamilyAttribute);
-            Assert.Null(tag.Attributes[4].Value);
-            Assert.Equal(TagFamilyEnum.NoEnd, tag.TagFamily);
-            Assert.Equal("<input type=\"radio\" id=\"rgender\" name=\"gender\" value=\"undefined\" checked/>", tag.TagStart);
-            Assert.Null(tag.TagEnd);
-            Assert.Null(tag.Children);
+            AssertFormPage.AssertRadioUndefined(tag);
         }
 
         [Fact]
@@ -88,16 +52,7 @@ namespace FalconEngineTest.DomParsing.Parser
 
             var tag = inputTagParser.Parse(html);
 
-            Assert.Equal(NameTagEnum.input, tag.NameTag);
-            Assert.Null(tag.Content);
-            Assert.Equal(FamilyAttributeEnum.type.ToString(), tag.Attributes[0].FamilyAttribute);
-            Assert.Equal("text", tag.Attributes[0].Value);
-            Assert.Equal(FamilyAttributeEnum.placeholder.ToString(), tag.Attributes[1].FamilyAttribute);
-            Assert.Equal("FirstName", tag.Attributes[1].Value);
-            Assert.Equal(TagFamilyEnum.NoEnd, tag.TagFamily);
-            Assert.Equal("<input type=\"text\" placeholder=\"FirstName\">", tag.TagStart);
-            Assert.Null(tag.TagEnd);
-            Assert.Null(tag.Children);
+            AssertFormPage.AssertInputFirstName(tag);
         }
     }
 }
