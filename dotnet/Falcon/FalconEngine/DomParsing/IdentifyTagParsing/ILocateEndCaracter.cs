48ac9e0719001540d3326b052e5892f156a88748
plouiserre <plouiserre@gmail.com>
2025-09-15 11:54:14 +0200

    localteendcaracter developed and test

---
+++ dotnet/Falcon/FalconEngine/DomParsing/IdentifyTagParsing/ILocateEndCaracter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FalconEngine.DomParsing.IdentifyTagParsing
+{
+    public interface ILocateEndCaracter
+    {
+        public int? Search(string startTag, string html);
+    }
+}
