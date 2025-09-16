80e09c2e941fa4b0f50014cc3c28aa470179856c
plouiserre <plouiserre@gmail.com>
2025-09-16 10:55:01 +0200

    write all big check test in div parser and comment it because for the moment it is ko

--- dotnet/Falcon/FalconEngineTest/DomParsing/Parser/Structure/DivParserTest.cs
+++ dotnet/Falcon/FalconEngineTest/DomParsing/Parser/Structure/DivParserTest.cs
@@ -7,7 +7,7 @@ using FalconEngineTest.Utils;
 using FalconEngineTest.Utils.AssertHtml;
 using FalconEngineTest.Utils.HtmlData;
 
-namespace FalconEngineTest.DomParsing.Parser
+namespace FalconEngineTest.DomParsing.Parser.Structure
 {
     public class DivParserTest
     {
@@ -59,5 +59,35 @@ namespace FalconEngineTest.DomParsing.Parser
 
             Assert.False(isValid);
         }
+
+        //[Fact]
+        //public void ParseMultipleDivValidateIt()
+        //{
+        //    string html = "<div class=\"main\"><div class=\"second\"><p>Hello World!!!</p></div></div>";
+
+        //    var divParser = TestFactory.InitDivParser();
+
+        //    var tag = divParser.Parse(html);
+        //    bool isValid = divParser.IsValid();
+
+        //    Assert.Equal(NameTagEnum.div, tag.NameTag);
+        //    Assert.Equal("<div class=\"main\">", tag.TagStart);
+        //    Assert.Equal("</div>", tag.TagEnd);
+        //    Assert.Equal("<div class=\"second\"><p>Hello World!!!</p></div>", tag.Content);
+        //    Assert.Equal(FamilyAttributeEnum.classCss.ToString(), tag.Attributes[0].FamilyAttribute);
+        //    Assert.Equal("main", tag.Attributes[0].Value);
+        //    Assert.Equal(NameTagEnum.div, tag.Children[0].NameTag);
+        //    Assert.Equal("<div class=\"second\">", tag.Children[0].TagStart);
+        //    Assert.Equal("</div>", tag.Children[0].TagEnd);
+        //    Assert.Equal("<p>Hello World!!!</p>", tag.Children[0].Content);
+        //    Assert.Equal(FamilyAttributeEnum.classCss.ToString(), tag.Children[0].Attributes[0].FamilyAttribute);
+        //    Assert.Equal("second", tag.Attributes[0].Value);
+        //    Assert.Equal(NameTagEnum.p, tag.Children[0].NameTag);
+        //    Assert.Equal("<p>", tag.Children[0].Children[0].TagStart);
+        //    Assert.Equal("</p>", tag.Children[0].Children[0].TagEnd);
+        //    Assert.Equal("Hello World!!!", tag.Children[0].Children[0].Content);
+        //    Assert.Null(tag.Children[0].Children[0].Attributes);
+        //    Assert.True(isValid);
+        //}
     }
 }
